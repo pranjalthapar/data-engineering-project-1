fc9418c7ef0ed84dd9bc3051e2155b2a680f1a07
pranjal1408@gmail.com <pranjal1408@gmail.com>
2024-04-10 13:31:04 +0000

    adding fct order file

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -3,21 +3,30 @@ with orders as
     select * from {{ ref ("stg_orders") }}
 ),
 
-payment as 
+payments as 
 (
     select * from {{ ref ("stg_payments") }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when payment_status = 'success' then amount end) as amount
+    from payments
+    group by 1
 ),
 
 final as 
 (
     select
-    order_id, 
-    customer_id,
-    amount
-    from
-    orders
+    orders.order_id, 
+    orders.customer_id,
+    orders.order_date,
+    coalesce(order_payments.amount, 0) as amount
+
+    from orders
     left join
-    payment using (order_id)
+    order_payments using (order_id)
 ) 
 
 select * from final
